312763acbdbf5e82b3851733083472f56b116ad7
Jani Halmetoja <jani.halmetoja@gmail.com>
2016-08-16 09:55:08 +0300

    updated package versions

--- package.js
+++ package.js
@@ -1,19 +1,20 @@
 Package.describe({
   summary: "Cache the counts of an associated collection",
-  version: "0.0.4",
+  version: "0.0.5",
   git: "https://github.com/hazio/meteor-counter-cache.git"
 });
 
 Package.onUse(function(api) {
   api.use([
-    'matb33:collection-hooks@0.7.11',
-    'underscore@1.0.0',
-    'mongo@1.0.6'
+    'matb33:collection-hooks@0.8.3',
+    'underscore@1.8.3',
+    'mongo@2.2.5'
   ]);
   api.add_files('counter-cache.js', ['client', 'server']);
 });
 
 Package.onTest(function(api) {
-  api.use(['tinytest', 'dburles:counter-cache']);
+  api.use(['tinytest', 'underscore', 'mongo', 'matb33:collection-hooks@0.8.3']);
   api.add_files('counter-cache_tests.js', 'server');
+  api.add_files('counter-cache.js', ['client', 'server']);
 });
